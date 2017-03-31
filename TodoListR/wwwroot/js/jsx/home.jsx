442e6673e0c66fd393ba649725827239ba4e28a0
Farland <farlandchen@eslite.com>
2017-03-31 16:08:48 +0800

    Reactive state 給了CommentBox一個初始化的屬性：data，並且將CommentList的資料來源指向CommentBox的data。
    this.state是元件本身的一個私有屬性，而且可以被this.setState()改變，當資料改變的時候元件會執行re-renders
    
    render() methods are written declaratively as functions of this.props and this.state. The framework guarantees the UI is always consistent with the inputs.
    
    getInitialState() 每個玩件生命週期只會執行一次。

--- TodoListR/wwwroot/js/jsx/home.jsx
+++ TodoListR/wwwroot/js/jsx/home.jsx
@@ -51,16 +51,20 @@ var CommentForm = React.createClass({
 });
 
 var CommentBox = React.createClass({
+    getInitialState: function () {
+        return { data: [] };
+    },
     render: function () {
         return (
             <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList data={this.props.data} />
+                <CommentList data={this.state.data} />
                 <CommentForm />
             </div>
         );
     }
 });
+
 ReactDOM.render(
     <CommentBox url="/comments" />,
     document.getElementById('content')
