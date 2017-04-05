f4e27d7ddb9b652b937e478f6cf84f4742ef9099
Farland <farlandchen@eslite.com>
2017-04-05 11:55:14 +0800

    加入TodoModel

---
+++ TodoListR/Models/TodoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoListR.Models
+{
+    public class TodoModel
+    {
+        public int ID { get; set; }
+        public string TodoTitle { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
